eebc2d17acaeeaa324292d344f681151d3fdf236
rishwanriju <rishwan619@gmail.com>
2021-03-30 13:06:42 +0530

    deleting added

--- src/App.js
+++ src/App.js
@@ -6,19 +6,27 @@ class App extends Component {
 
   state = {
     person : [
-    {name: 'riz' , age: '20'},
-    {name: 'manu' , age: '42'}
-  ] }
+    {id : "1",name: 'riz' , age: '20'},
+    {id : "2",name: 'manu' , age: '42'}
+  ],
+  showPerson : false
+}
 
-  switchNameHandler = (newName) => {
-    // console.log("clicked")
+  deletePersonHandler = (personIndex) => {
+    
+    // const person = this.state.person.slice()
+    const person = [...this.state.person];
+    person.splice(personIndex, 1);
     this.setState({
-      person : [
-        {name: newName , age: '20'},
-        {name: 'manu' , age: '20'}
-      ]
+      person: person
+    });
 
-    })
+
+  }
+
+  ToggleNameHandler = () => {
+    const doesShow = this.state.showPerson
+    this.setState({showPerson: !doesShow})
   }
 
   nameChangeHandler = (updatedname) => {
@@ -40,18 +48,37 @@ class App extends Component {
       padding : '10px',
       cusor : 'pointer',
     };
-    
+  
+    let person = null;
+
+    if (this.state.showPerson){
+      person = (
+        <div >
+          {this.state.person.map((pers,index) => {
+            return <Person
+            click =  {()=>this.deletePersonHandler(index)}
+            name = {pers.name}
+            age = {pers.age}
+            key = {pers.id}
+            />
+
+     })}
+       </div> 
+      )
+    }
     return (
       <div className="App">
       <h3>my new app</h3>
       <p>my new app is working</p>
-      <button style={styles} onClick={() => this.switchNameHandler("rizvan")}>switch name</button>
-      <Person change={this.nameChangeHandler} name = {this.state.person[0].name} age = {this.state.person[0].age}>my hobbie is working </Person>
-      <Person click={this.switchNameHandler.bind(this,"rishwan")} name = {this.state.person[1].name} age = {this.state.person[1].age} >my hobbie is jogging </Person>
+      <button style={styles} onClick= { () =>this.ToggleNameHandler()}>toggle name</button>
+
+      {person}     
+        
       </div>
-    // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'my name is riz'))
+    // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'my id : ""name is riz'))
     );
   }
 }
 
 export default App;
+ 
